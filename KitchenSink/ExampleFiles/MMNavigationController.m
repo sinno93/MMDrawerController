c48682cf4efdc00f0f192c80a7a3a010645cd05f
Sinno <sinno93@qq.com>
2019-07-07 08:47:11 +0800

    1.添加一些声明周期方法测试

--- KitchenSink/ExampleFiles/MMNavigationController.m
+++ KitchenSink/ExampleFiles/MMNavigationController.m
@@ -35,4 +35,24 @@
     }
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    NSLog(@"%s",__FUNCTION__);
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    NSLog(@"%s",__FUNCTION__);
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    NSLog(@"%s",__FUNCTION__);
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    NSLog(@"%s",__FUNCTION__);
+}
+
 @end
