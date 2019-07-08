922050d81f52c590f15d6a4325774cc89e911258
Sinno <sinno93@qq.com>
2019-07-09 00:46:47 +0800

    1.增加了滑动动画

---
+++ KitchenSink/ExampleFiles/MyLeftViewController.h
@@ -0,0 +1,17 @@
+//
+//  MyLeftViewController.h
+//  MMDrawerControllerKitchenSink
+//
+//  Created by Sinno on 2019/7/7.
+//  Copyright © 2019 Mutual Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyLeftViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KitchenSink/ExampleFiles/MyLeftViewController.m
@@ -0,0 +1,32 @@
+//
+//  MyLeftViewController.m
+//  MMDrawerControllerKitchenSink
+//
+//  Created by Sinno on 2019/7/7.
+//  Copyright © 2019 Mutual Mobile. All rights reserved.
+//
+
+#import "MyLeftViewController.h"
+
+@interface MyLeftViewController ()
+
+@end
+
+@implementation MyLeftViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
